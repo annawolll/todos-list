e889d7f7005ea91f77698cc45ee8cfce764c0d71
Anna Wołoszyn <anna.woloszyn@itsilesia.com>
2024-07-28 19:40:03 +0200

    feat: initialize project

---
+++ todos-list/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { HomePage } from './screens/HomePage/HomePage'
+
+function App() {
+  return <HomePage />
+}
+
+export default App
---
+++ todos-list/src/screens/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+  return (
+    <div className="relative flex flex-col w-full">
+      <img src="bg-desktop-light.jpg" className="w-full" />
+      <div className="bg-gray-200" />
+      <div className="absolute">TODO </div>
+    </div>
+  )
+}
